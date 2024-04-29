dc69f7117d494c14cb789910c9ea5023216e2b1c
Sabbir Hossain <cse.170201039@gmail.com>
2024-04-29 21:14:27 +0600

    api.js added to .gitignore

--- src/api/api.js
+++ src/api/api.js
@@ -3,11 +3,11 @@ export const geo_url = 'https://wft-geo-db.p.rapidapi.com/v1/geo/adminDivisions'
 export const geo_options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'GEO_API_KEY',
+    'X-RapidAPI-Key': '5e5804ea53msh791fda2ff7be537p1231abjsn6dcd70830d21',
     'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
   }
 };
 
 export const weather_api_url = "https://api.openweathermap.org/data/2.5/";
 
-export const weather_api_key = "WEATHER_API_KEY";
+export const weather_api_key = "7174a59b54838232230b5daaffaffb44";
